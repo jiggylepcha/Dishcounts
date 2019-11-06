034bf69c0776c311bc351d101deb493ecc75a06e
Vaasu Gupta <vaasu16275@iiitd.ac.in>
2019-11-06 07:29:13 +0530

    Fragments for UI additions and changes

--- app/src/main/java/android/com/dishcounts/Fragments/AddNewFragment.java
+++ app/src/main/java/android/com/dishcounts/Fragments/AddNewFragment.java
@@ -1,6 +1,8 @@
 package android.com.dishcounts.Fragments;
 
+import android.com.dishcounts.Activities.AddManualCouponActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,14 +13,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.com.dishcounts.R;
+import android.widget.Button;
 
 
 public class AddNewFragment extends Fragment {
+    Button add;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new, container, false);
+        View v = inflater.inflate(R.layout.fragment_add_new, container, false);
+        add = v.findViewById(R.id.button31);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAdd();
+            }
+        });
+        return v;
+    }
+
+    public void openAdd(){
+        Intent intent = new Intent(this.getContext(), AddManualCouponActivity.class);
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/android/com/dishcounts/Fragments/CouponFragment.java
+++ app/src/main/java/android/com/dishcounts/Fragments/CouponFragment.java
@@ -1,6 +1,8 @@
 package android.com.dishcounts.Fragments;
 
+import android.com.dishcounts.Activities.CouponActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,13 +13,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.com.dishcounts.R;
+import android.widget.Button;
 
 public class CouponFragment extends Fragment {
+    Button coupon;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coupon, container, false);
+        View v = inflater.inflate(R.layout.fragment_coupon, container, false);
+        coupon = v.findViewById(R.id.button2);
+        coupon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCoupon();
+            }
+        });
+        return v;
+    }
+
+    public void openCoupon(){
+        Intent intent = new Intent(this.getContext(), CouponActivity.class);
+        startActivity(intent);
     }
 
     @Override
